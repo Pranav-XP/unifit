e2f18c6b9af18398b7878187164be63fe2203c74
Pranav <112617967+Pranav-XP@users.noreply.github.com>
2024-08-26 22:23:07 +1200

    started project

---
+++ src/main/java/dev/forge/unifit/UnifitApplication.java
@@ -0,0 +1,13 @@
+package dev.forge.unifit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnifitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnifitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/forge/unifit/UnifitApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.forge.unifit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnifitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
