7f22bb0de65abc88fc3172630a6fa265ea00b9e8
Pranav <112617967+Pranav-XP@users.noreply.github.com>
2024-10-23 00:46:35 +1200

    updated homepage to have facility displayed

--- src/main/java/dev/forge/unifit/view/ViewController.java
+++ src/main/java/dev/forge/unifit/view/ViewController.java
@@ -1,24 +1,27 @@
 package dev.forge.unifit.view;
 
 import dev.forge.unifit.email.EmailService;
+import dev.forge.unifit.facility.Facility;
+import dev.forge.unifit.facility.FacilityService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/")
 @RequiredArgsConstructor
 public class ViewController {
     private final EmailService email;
+    private final FacilityService facilityService;
 
     @GetMapping()
-    public String homePage(){
+    public String homePage(Model model){
+        List<Facility> facilities = facilityService.getAllFacilities();
+        model.addAttribute("facilities",facilities);
         return "index";
     }
 
